18a9d8ed86db22fc84f5a102ac45599391c06298
efrainlim <efrainlima7@gmil.com>
2025-07-27 17:25:25 -0500

    commit restore

--- sistema_comercial/settings.py
+++ sistema_comercial/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 CSRF_TRUSTED_ORIGINS = [
     'https://minerafidami.me',
